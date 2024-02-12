cdb9a2980ca71f31e7ef645444f9e23e316b49cf
Jonathan Velosa <jonathan_velosa@epam.com>
2024-02-12 16:27:46 -0500

    Task 3 implemented
    New project was created to call the API

---
+++ Caller/Program.cs
@@ -0,0 +1,21 @@
+﻿const string apiUrl = "http://localhost:5096/api/Product?pageNumber=1&pageSize=10";
+
+using var client = new HttpClient();
+try
+{
+    var response = await client.GetAsync(apiUrl);
+
+    if (response.IsSuccessStatusCode)
+    {
+        var result = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine($"An error has occurred: {response.StatusCode} - {response.ReasonPhrase}");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
